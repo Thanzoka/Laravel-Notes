0c0d7b3607edc3445704af89e9fcf08c7af9dfb5
ThanZoka <bob.juniorlud@hotmail.com>
2026-01-08 14:21:19 -0300

    006

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,4 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MainController extends Controller {}
+class MainController extends Controller
+{
+    public function index()
+    {
+        echo "I'm inside the app!";
+    }
+
+    public function newNote()
+    {
+        echo "Create a new note!";
+    }
+}
